66d012cacfc7c9cea6be51dccf7cea9c731e579c
Craig G <gibson.craig05@gmail.com>
2023-09-17 21:29:53 +0100

    various changes, adding in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,64 @@ $(document).ready(function(){
 
     // listen for save button clicks
 
-    $(".saveBtn");
+    $(".saveButton").on("click", function(){
+        console.log(`it saved`);
+
+        let timeID = $(this).parent().attr("id")
+        let value = $(this).siblings(".description").val();
+
+        console.log(timeID);
+        console.log(value);
+
+        localStorage.setItem(timeID, value);
+
+        $(".notification").addClass('show');
+
+
+        setTimeout(function(){
+            $("notification").removeClass("show")
+        }, 5000)
+
+
+    })
+
+
+    function hourUpdate() {
+        let currentHour = moment().hours();
+
+
+        for(let i = 0; i < $(".time-block").length; i++){
+            let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1]);
+            console.log(hour)
+            console.log(currentHour)
+            if(hour < currentHour) {
+                $(".time-block")[i].classList.add("past");
+            } else if(hour === currentHour){
+                $(".time-block")[i].classList.add("past");
+                $(".time-block")[i].classList.remove("present");
+            } else {
+                $(".time-block")[i].classList.remove("past");
+                $(".time-block")[i].classList.remove("present");
+                $(".time-block")[i].classList.add("future");
+
+            }
+
+            }
+        }
+
+
+
+
+    }
+
+
+    hourUpdate();
+
+    let inteveral = setInterval(hourUpdate, 15000);
+
+    $("hour-9 .description").val(localStorage.getItem("hour-9"))
+
+    $("#currentDay").text(moment().format("dddd,MM, Do"))
+
+
 })
